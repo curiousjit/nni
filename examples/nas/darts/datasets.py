692df7e468c1be118b6581efa190e15f83b99dd7
godfather <satyajitpadhyewit@gmail.com>
2020-06-22 12:08:55 +0800

    Update datasets.py

--- examples/nas/darts/datasets.py
+++ examples/nas/darts/datasets.py
@@ -3,9 +3,14 @@
 
 import numpy as np
 import torch
+import os
+import natsort
+import pandas as pd
+import torch
 from torchvision import transforms
+from PIL import Image
 from torchvision.datasets import CIFAR10
-
+from torch.utils.data import Dataset, DataLoader, random_split
 
 class Cutout(object):
     def __init__(self, length):
@@ -29,6 +34,23 @@ class Cutout(object):
 
         return img
 
+class CustomDataSet(Dataset):
+    def __init__(self, csv_file,main_dir, transform):
+        self.label_frame = pd.read_csv(csv_file,usecols=["Label"])
+        self.main_dir = main_dir
+        self.transform = transform
+        all_imgs = os.listdir(main_dir)
+        self.total_imgs = natsort.natsorted(all_imgs)
+
+    def __len__(self):
+        return len(self.total_imgs)
+
+    def __getitem__(self, idx):
+        img_loc = os.path.join(self.main_dir, self.total_imgs[idx])
+        image = Image.open(img_loc).convert("RGB")
+        tensor_image = self.transform(image)
+        labels = self.label_frame.iloc[idx, 0]
+        return (tensor_image,labels)
 
 def get_dataset(cls, cutout_length=0):
     MEAN = [0.49139968, 0.48215827, 0.44653124]
@@ -47,10 +69,23 @@ def get_dataset(cls, cutout_length=0):
 
     train_transform = transforms.Compose(transf + normalize + cutout)
     valid_transform = transforms.Compose(normalize)
+    
+    #section for AOI
+    img_folder_path="/home/satyajit/nni/AOI/aoi/train_images"
+    label_file="/home/satyajit/nni/AOI/aoi/train.csv"
+
+    trsfm = transforms.Compose([transforms.Resize((32,32)),transforms.CenterCrop(32),transforms.ToTensor()])
+
+    my_dataset = CustomDataSet(label_file,img_folder_path,transform=trsfm)
+
+    trainset, valset = random_split(my_dataset, [2026, 502])
 
     if cls == "cifar10":
         dataset_train = CIFAR10(root="./data", train=True, download=True, transform=train_transform)
         dataset_valid = CIFAR10(root="./data", train=False, download=True, transform=valid_transform)
+    elif cls=="aoi":
+        dataset_train =trainset 
+        dataset_valid =valset
     else:
         raise NotImplementedError
     return dataset_train, dataset_valid
